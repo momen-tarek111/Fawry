8ccdb4f68063000e744d9fec6cc5e454c2338505
momen-tarek111 <110129557+momen-tarek111@users.noreply.github.com>
2025-07-05 23:46:20 +0300

    Finally

--- src/SuperMarket/Admin.java
+++ src/SuperMarket/Admin.java
@@ -4,7 +4,5 @@ public class Admin extends User{
     public Admin(String userName,String email,String password,boolean isAdmin){
         super(userName,email,password,isAdmin);
     }
-//    public Product addProduct(){
-//        System.out.println("Enter name of dish :");
-//    }
+
 }
